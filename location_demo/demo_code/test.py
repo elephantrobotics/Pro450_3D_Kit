23b3d2ed7562b27017816124d1a5900f37b14855
TheFive666 <116151187+TheFive666@users.noreply.github.com>
2026-08-17 10:01:19 +0800

    Update test.py

--- location_demo/demo_code/test.py
+++ location_demo/demo_code/test.py
@@ -7,8 +7,8 @@ if er.is_power_on()!=1:
 
 er.set_end_type(0)
 try:
-    er.send_angles([90,0,0,0,0,0],10)
     er.send_angles([0,0,0,0,0,0],10)
+    er.send_angles([26.8, 25.04, -102.13, -15.94, -0.58, 72.2],10)
     er.set_base_io_output(1,1)
     time.sleep(2)
     er.set_base_io_output(1,0)
